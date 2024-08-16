6075c51a5d995c6b99ad3070c7a00ee6add97310
JOANA CASTELLÓ VISIEDO <castellovisiedo@gmail.com>
2024-08-17 01:56:16 +0200

    feat(style): Implement buttons styles

---
+++ app/buttons/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const ButtonsPage = () => {
+  return (
+    <div className='p-4 space-y-4'>
+      <Button>Default</Button>
+      <Button variant="defaultOutline">Default outline</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary outline</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger outline</Button>
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super outline</Button>
+      <Button variant="sidebar">Sidebar</Button>
+      <Button variant="sidebarOutline">Sidebar outline</Button>
+    </div>
+  )
+}
+
+export default ButtonsPage
